160b74f3b446746cb01b7b6fbc2069fdb5bf3bab
Dongliang Mu <mudongliangabcd@gmail.com>
2018-03-17 09:13:46 -0400

    catch all exceptions in python

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # markdown_scripts
 Some scripts to handle markdown files in Linux-insides-zh
+
+## Description
+
+`get_all_links.py` : justify one link is live or dead with network connection
+
+## Usage
+
+`get_all_links.py` : `./get_all_links.py ../`
--- get_all_links.py
+++ get_all_links.py
@@ -14,21 +14,27 @@ try:
     # compatible for python2
     from urllib2 import urlopen
     from urllib2 import HTTPError
+    from urllib2 import URLError
 except ImportError:
     # compatible for python3
     from urllib.request import urlopen
     from urllib.error import HTTPError
+    from urllib.error import URLError
 
 def check_live_url(url):
 
     result = False
     try:
-        ret = urlopen(url, timeout=5)
+        ret = urlopen(url, timeout=2)
         result = (ret.code == 200)
     except HTTPError as e:
-        print(e.code, file=sys.stderr)
+        print(e, file=sys.stderr)
+    except URLError as e:
+        print(e, file=sys.stderr)
     except timeout as e:
-        print(e.code, file=sys.stderr)
+        print(e, file=sys.stderr)
+    except Exception as e:
+        print(e, file=sys.stderr)
 
     return result
 
@@ -48,11 +54,15 @@ def main(path):
         for line in fd.readlines():
             refs = re.findall(r'(?<=<a href=")[^"]*', markdown.markdown(line))
             for ref in refs:
-                urls.append(ref)
+                if ref not in urls:
+                    urls.append(ref)
 
-    #print(urls)
+    #print(len(urls))
 
     for url in urls:
+        if not url.startswith("http"):
+            print("markdown file name: " + url)
+            continue
         if check_live_url(url):
             print(url)
         else:
